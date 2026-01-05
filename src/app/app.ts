a65c0f604a52337f741aed2bb18ae51b161cc52f
Viviana Ospina <vyvy_escobar@hotmail.com>
2026-01-05 12:37:20 -0500

    concepto de interpolacion en Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './new-component/new-component';
+import { InterpolacionComponent } from './interpolacion/interpolacion';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponent],
+  imports: [RouterOutlet, NewComponent, InterpolacionComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/interpolacion/interpolacion.ts
+++ src/app/interpolacion/interpolacion.ts
@@ -2,10 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-interpolacion',
+  standalone: true,
   imports: [],
   templateUrl: './interpolacion.html',
   styleUrl: './interpolacion.css',
 })
-export class Interpolacion {
+export class InterpolacionComponent {
+  title = 'Universidad angular';
+  user = {
+    name: 'viviana ospina',
+    age: 36,
+  };
 
+  saludar() {
+    return `Hola, ${this.user.name}`;
+  }
 }
