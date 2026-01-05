dffd91f095ae3e819d58ed585fad69f49ab95598
Viviana Ospina <vyvy_escobar@hotmail.com>
2026-01-05 17:19:17 -0500

    Separar información de productos en componentes independientes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
+import { ProductComponent } from './product/product';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InterpolacionComponent } from './interpolacion/interpolacion';
+import { ListProducts } from './list-products/list-products';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InterpolacionComponent],
+  imports: [RouterOutlet, InterpolacionComponent, ProductComponent, ListProducts],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
